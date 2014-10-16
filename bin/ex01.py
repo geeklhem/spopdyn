d75f8f38b77f8a8a541505ba93282e6b227381b5
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-10-16 11:56:43 +0200

    multi ex01

--- bin/ex01.py
+++ bin/ex01.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 temperature = np.zeros((2,2))+0.5
 habitat = np.zeros((2,2))+0.5
-species = np.array([(0.5,0.5,0.1,0.1,0),(0.5,0.6,0.1,0.1,0)])
+
 param = {"alpha":1,
          "name":"2sp_80pts",
           "d":1,
@@ -17,19 +17,38 @@ param = {"alpha":1,
           "m":1e-1,
           "T_range":np.linspace(0,0.2,40),
           }
+
+species = []
+outputs = []
+species.append(np.array([(0.5,0.5,0.1,0.1,0),(0.5,0.6,0.1,0.1,0)]))
+outputs.append(applyDT(habitat,temperature,species[-1],param))
 dt = (species[1,1]**2-species[0,1]**2)/(2.*(species[1,1]-species[0,1])) - temperature[0,0]
-out = applyDT(habitat,temperature,species,param)
+
+param["name"] = "2sp_80pts_s02"
+species.append(np.array([(0.5,0.5,0.2,0.2,0), (0.5,0.6,0.2,0.2,0)]))
+outputs.append(applyDT(habitat,temperature,species[-1],param))
+
+param["name"] = "5sp_160pts"
+param["T_range"] = np.linspace(0,0.4,80)
+species.append(np.array([(0.5,0.3,0.2,0.2,0), (0.5,0.4,0.2,0.2,0),
+                         (0.5,0.5,0.2,0.2,0), (0.5,0.6,0.2,0.1,0),
+                         (0.5,0.7,0.2,0.1,0)]))
+outputs.append(applyDT(habitat,temperature,species[-1],param))
+
+
 color = "rb"
 
-plt.figure(figsize=(10,5))
-plt.subplot(121)
-d.niches(species)
-plt.subplot(122)
-plt.xlabel("$\Delta T$")
-plt.ylabel("$\Delta CTI$")
-plt.scatter(out["deltaT"],out["deltaCTI"],c=[color[x<0] for x in out["deltaT"]])
-plt.hlines(0,*plt.xlim())
-plt.vlines(0,*plt.ylim())
-plt.vlines(dt,*plt.ylim())
-plt.savefig("ex01.png")
-plt.savefig("ex01.eps")
+plt.figure(figsize=(10,N*5))
+N = len(outputs)
+for n,(out,sp) in enumerate(zip(outputs,species)):
+    plt.subplot(1,N,1+n)
+    d.niches(sp)
+    plt.subplot(1,N,2+n)
+    plt.xlabel("$\Delta T$")
+    plt.ylabel("$\Delta CTI$")
+    plt.scatter(out["deltaT"],out["deltaCTI"],c=[color[x<0] for x in out["deltaT"]])
+    plt.hlines(0,*plt.xlim())
+    plt.vlines(0,*plt.ylim())
+    plt.vlines(dt,*plt.ylim())
+plt.savefig("ex_01_m.png")
+plt.savefig("ex_01_m.eps")
