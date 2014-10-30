5a81bdcff470762dac5eb7e5ce95403d900a3352
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-10-30 12:29:40 +0100

    colors and vertical bar upgrade

--- experiments/2014-10-30/make.py
+++ experiments/2014-10-30/make.py
@@ -1,6 +1,6 @@
 """
 DCTI = F(T) for two different habitats. 
-"""778
+"""
 from __future__ import division
 import numpy as np
 import spopdyn.display as d
@@ -26,20 +26,19 @@ species = []
 outputs = []
 dt = []
 pts = []
+tc = []
 
-def tc_2habs(T, muH1,muH2,muT1,muT2):
+def tc_2habs(muH1,muH2,muT1,muT2):
     """Return Tc for H=muH1 et H=muH2"""
     mid = muT1+muT2 / 2   
     tc_muH1 = (muH1 - muH2)**2 / (2*(muT2 - muT1))
     tc_muH2 = (muH2 - muH1)**2 / (2*(muT1 - muT2))
     tc_muH1 += mid
     tc_muH2 += mid 
-    tc_muH1 -= T
-    tc_muH2 -= T 
     return (tc_muH1,tc_muH2)
 
-def tc_2sp(T,muT1,muT2):
-    return (muT1 + muT2)/2 - T
+def tc_2sp(muT1,muT2):
+    return (muT1 + muT2)/2 
 
 
 ## 2 SP. equal sigma, diff muH. 
@@ -51,10 +50,9 @@ species.append(np.array([(0.5,0.5,0.1,0.1,0),
                          (0.6,0.6,0.1,0.1,0)]))
 outputs.append(applyDT(habitat,temperature,species[-1],param))
 pts.append(([0.5,0.5],[0.5,0.6]))
-dt.append(tc_2habs(temperature[0,0],
-                   species[-1][0,0], species[-1][1,0],
+tc.append(tc_2habs(species[-1][0,0], species[-1][1,0],
                    species[-1][0,1], species[-1][1,1]))
-
+dt.append([x-temperature[0,0] for x in tc[-1]])
 
 ## 2 SP. equal sigma, diff muH. 
 param["name"] = "EX04_02"
@@ -65,10 +63,9 @@ species.append(np.array([(0.5,0.5,0.1,0.1,0),
                          (0.6,0.8,0.1,0.1,0)]))
 outputs.append(applyDT(habitat,temperature,species[-1],param))
 pts.append(([0.5,0.5],[0.5,0.8]))
-dt.append(tc_2habs(temperature[0,0],
-                   species[-1][0,0], species[-1][1,0],
+tc.append(tc_2habs(species[-1][0,0], species[-1][1,0],
                    species[-1][0,1], species[-1][1,1]))
-
+dt.append([x-temperature[0,0] for x in tc[-1]])
 
 ## 4 SP. equal sigma, diff muH. 
 param["name"] = "EX04_03"
@@ -81,25 +78,28 @@ species.append(np.array([(0.4,0.5,0.1,0.1,0),
                          (0.6,0.6,0.1,0.1,0)]))
 outputs.append(applyDT(habitat,temperature,species[-1],param))
 pts.append(([0.5,0.5],[0.4,0.6]))
-dt.append((tc_2sp(temperature[0,0],species[-1][0,1],species[-1][2,1]),
+tc.append((tc_2sp(temperature[0,0],species[-1][0,1],species[-1][2,1]),
            tc_2sp(temperature[0,0],species[-1][1,1],species[-1][3,1])))
-
+dt.append([x-temperature[0,0] for x in tc[-1]])
 
 color = "rb"
 N = len(outputs)
 plt.figure(figsize=(10,N*5))
 
-for n,(out,sp,dti,pt) in enumerate(zip(outputs,species,dt,pts)):
+for n,(out,sp,tci,dti,pt) in enumerate(zip(outputs,species,tc,dt,pts)):
     plt.subplot(N,2,2*n+1)
     d.niches(sp)
     plt.scatter(*pt)
+    plt.vlines(tci,*plt.ylim(),linestyle=":",color="darkblue")
     plt.subplot(N,2,2*(n+1))
     plt.xlabel("$\Delta T$")
     plt.ylabel("$\Delta CTI$")
     plt.scatter(out["deltaT"],out["deltaCTI"],c=[color[x<0] for x in out["deltaT"]])
     plt.hlines(0,*plt.xlim())
     plt.vlines(0,*plt.ylim())
-    plt.vlines(dti,*plt.ylim(),color="grey")
+    plt.vlines(dti,*plt.ylim(),linestyle=":",color="darkblue")
+plt.tight_layout()
+
 plt.savefig("dt_dcti_2habs.png")
 plt.savefig("dt_dcti_2habs.eps")
 metadata("dt_dcti_2habs.png")
